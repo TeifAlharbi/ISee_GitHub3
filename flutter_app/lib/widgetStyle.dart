6c063e78c6791d8fa6b8fdc2344117e8606b9118
bayanfbaz <78942439+bayanfbaz@users.noreply.github.com>
2021-04-05 22:09:54 +0300

    simple test fix

---
+++ flutter_app/lib/widgetStyle.dart
@@ -0,0 +1,20 @@
+//ALL DONE
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:adobe_xd/pinned.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+//abstract class widgetStyle extends StatelessWidget {
+class widgetStyle extends StatelessWidget {
+  //const ColorPicker({this.onColorSelect});
+
+  //final ColorCallback onColorSelect;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+  }
+}
+
+//typedef ColorCallback = void Function(Color color);
--- flutter_app/test/testPlate_test.dart
+++ flutter_app/test/testPlate_test.dart
@@ -1,15 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_app/signIn.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_app/aboutISee.dart';
-import 'package:flutter_app/Camera.dart';
-import 'package:flutter_app/signUp.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mockito/mockito.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:flutter_app/testPlate1.dart';
 
 
@@ -55,11 +45,6 @@ main(){
     //check if the actual result matches the expected result
     expect(value,false);
   });
-
-
-
-
-
 
 }
 
